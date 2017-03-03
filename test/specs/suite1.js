d69d6b0d9c374013bfb3694f187aa78f52af130c
Christopher King <chrisking1@gmail.com>
2017-03-02 21:39:36 -0500

    npm install jquery to try to get chai jquery to work

--- test/specs/suite1.js
+++ test/specs/suite1.js
@@ -1,4 +1,5 @@
 const webdriverio = require('webdriverio');
+const jquery      = require('jquery');
 const chai        = require('chai');
 const chaijquery  = require('chai-jquery');
 const should      = chai.should;
@@ -17,39 +18,43 @@ describe('suite 1 tests', function(){
 
     it('Furniture test',function() {
         return client
-            .url('https://www.1stdibs.com/')
+            .url('https://www.1stdibs.com/').then(function (result) {
             //more methods here
-            .click('#mainNavLinkFurniture').then(function (result) {
-                expect('.browse-header-title').to.equal('Furniture');
+                expect($('#mainNavLinkFurniture')).to.have.class('global-nav-item');
             });
+
+            // .click('#mainNavLinkFurniture').then(function (result) {
+                // expect('.browse-header-title').to.equal('Furniture');
+                // expect($('.browse-header-title')).to.have.text('Furniture');
+            // });
     });
 
-    it('Fine Art test',function() {
-        return client
-            .url('https://www.1stdibs.com/')
-            //more methods here
-            .click('#mainNavLinkFineArt').then(function (result) {
-                expect('.browse-header-title').to.equal('Fine Art');
-            });
-    });
+    // it('Fine Art test',function() {
+    //     return client
+    //         .url('https://www.1stdibs.com/')
+    //         //more methods here
+    //         .click('#mainNavLinkFineArt').then(function (result) {
+    //             expect('.browse-header-title').to.equal('Fine Art');
+    //         });
+    // });
 
-    it('Jewelry test',function() {
-        return client
-            .url('https://www.1stdibs.com/')
-            //more methods here
-            .click('#mainNavLinkJewelryWatches').then(function (result) {
-                expect('.browse-header-title').to.equal('Jewelry & Watches');
-            });
-    });
+    // it('Jewelry test',function() {
+    //     return client
+    //         .url('https://www.1stdibs.com/')
+    //         //more methods here
+    //         .click('#mainNavLinkJewelryWatches').then(function (result) {
+    //             expect('.browse-header-title').to.equal('Jewelry & Watches');
+    //         });
+    // });
 
-    it('Fashion test',function() {
-        return client
-            .url('https://www.1stdibs.com/')
-            //more methods here
-            .click('#mainNavLinkFashion').then(function (result) {
-                expect('.browse-header-title').to.equal('Fashion');
-            });
-    });
+    // it('Fashion test',function() {
+    //     return client
+    //         .url('https://www.1stdibs.com/')
+    //         //more methods here
+    //         .click('#mainNavLinkFashion').then(function (result) {
+    //             expect('.browse-header-title').to.equal('Fashion');
+    //         });
+    // });
 
     after(function() {
         return client.end();
