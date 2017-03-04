4fbcd8a166e6ac6b033636dc4a16616e089aaeb8
Christopher King <chrisking1@gmail.com>
2017-03-04 14:10:52 -0500

    log out after furniture test

--- test/specs/suite1.js
+++ test/specs/suite1.js
@@ -16,8 +16,6 @@ describe('suite 1 tests', function(){
             return client.init();
     });
 
-    //Create test login, save info in environmental variables, and have each test login beforehand so the modal doesn't pop up then logout afterward
-
     it('Furniture test',function() {
         return client
             .url('https://www.1stdibs.com/')
@@ -29,32 +27,39 @@ describe('suite 1 tests', function(){
             .setValue('#login_password','s!HNK6N776&9Rju')
             .pause(2000)
             .click("button[data-tn='auth-modal-login-submit-button']")
-            .pause(3000)
+            .pause(5000)
             .click("a[data-tn='global-nav-item-link-furniture']")
             .pause(10000)
             .getText('.browse-header-title').then(function (text) {
                 expect(text).to.include('Furniture');
             })
+            .pause(3000)
+            .click('.AccountNav-style__accountIcon__cVYjZ')
+            .pause(3000)
+            .click("span[data-tn='sub-nav-list-item-link-logout']")
+            .pause(5000)
+            .url('https://www.1stdibs.com/')
+            .pause(5000)
     });
 
-    // it('Fine Art test',function() {
-    //     return client
-    //         .url('https://www.1stdibs.com/')
-    //         .pause(2000)
-    //         .click("span[data-action='login']")
-    //         .pause(3000)
-    //         .setValue('#login_email','1stdibstest@gmail.com')
-    //         .pause(2000)
-    //         .setValue('#login_password','s!HNK6N776&9Rju')
-    //         .pause(2000)
-    //         .click("button[data-tn='auth-modal-login-submit-button']")
-    //         .pause(3000)
-    //         .click("a[data-tn='global-nav-item-link-fine art']")
-    //         .pause(10000)
-    //         .getText('.browse-header-title').then(function (text) {
-    //             expect(text).to.include('Fine Art');
-    //         })
-    // });
+    it('Fine Art test',function() {
+        return client
+            .url('https://www.1stdibs.com/')
+            .pause(2000)
+            .click("span[data-action='login']")
+            .pause(3000)
+            .setValue('#login_email','1stdibstest@gmail.com')
+            .pause(2000)
+            .setValue('#login_password','s!HNK6N776&9Rju')
+            .pause(2000)
+            .click("button[data-tn='auth-modal-login-submit-button']")
+            .pause(3000)
+            .click("a[data-tn='global-nav-item-link-fine art']")
+            .pause(10000)
+            .getText('.browse-header-title').then(function (text) {
+                expect(text).to.include('Fine Art');
+            })
+    });
 
     // it('Jewelry test',function() {
     //     return client
